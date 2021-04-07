4da1d230b31240baed68252d89eed63cb93fbd8b
David Redziniak <ec2-user@ip-172-31-35-129.ec2.internal>
2021-04-07 20:54:57 +0000

    Add activities relationship to User Model

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(DB.Model):
     first_name = DB.Column(DB.String(80), nullable=False)
     last_name = DB.Column(DB.String(80), nullable=False)
     trips = DB.relationship('UserTrip', backref='user', lazy=True)
+    activities = DB.relationship('UserActivity', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.id
@@ -23,7 +24,7 @@ class UserActivity(DB.Model):
     ''' UserActivity model '''
     id = DB.Column(DB.Integer, primary_key=True)
     user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
-    user_paid = DB.Column(DB.Boolean, unique=True, nullable=False)
+    user_paid = DB.Column(DB.Boolean, nullable=False)
 
     def __repr__(self):
         return '<UserActivity %r>' % self.id
