783944f76065c8561fce02f79f4db6a46f5996e0
David Redziniak <ec2-user@ip-172-31-35-129.ec2.internal>
2021-04-07 20:13:20 +0000

    Update models

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import os
 from flask import Flask, send_from_directory
+from dotenv import load_dotenv, find_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static_folder='./build/static')
+APP = Flask(__name__, static_folder='./build/static')
+DB = SQLAlchemy(APP)
+
+import models
+
+# Point SQLAlchemy to your Heroku database
+APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-@app.route('/', defaults={"filename": "index.html"})
-@app.route('/<path:filename>')
+@APP.route('/', defaults={"filename": "index.html"})
+@APP.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
 
-app.run(
+APP.run(
     host=os.getenv('IP', '0.0.0.0'),
     port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
 )
--- models.py
+++ models.py
@@ -1,1 +1,28 @@
 from app import DB
+
+class User(DB.Model):
+    ''' User model '''
+    id = DB.Column(DB.Integer, primary_key=True)
+    email = DB.Column(DB.String(80), unique=True, nullable=False)
+    first_name = DB.Column(DB.String(80), nullable=False)
+    last_name = DB.Column(DB.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+class UserTrip(DB.Model):
+    ''' UserTrip model '''
+    id = DB.Column(DB.Integer, primary_key=True)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<UserTrip %r>' % self.id
+
+class UserActivity(DB.Model):
+    ''' UserActivity model '''
+    id = DB.Column(DB.Integer, primary_key=True)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user_paid = DB.Column(DB.Boolean, unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<UserActivity %r>' % self.id
