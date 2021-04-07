9ab61ca0c7f772655a964798725bf560a606cd02
Mvye <mervyn.mathew18@gmail.com>
2021-04-07 22:58:14 +0000

    Created Trip and Activity models for database

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ from flask_sqlalchemy import SQLAlchemy
 load_dotenv(find_dotenv())  # This is to load your env variables from .env
 
 APP = Flask(__name__, static_folder='./build/static')
-DB = SQLAlchemy(APP)
-
-import models
 
 # Point SQLAlchemy to your Heroku database
 APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 # Gets rid of a warning
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+DB = SQLAlchemy(APP)
+
+import models
 
 @APP.route('/', defaults={"filename": "index.html"})
 @APP.route('/<path:filename>')
--- models.py
+++ models.py
@@ -6,25 +6,31 @@ class User(DB.Model):
     email = DB.Column(DB.String(80), unique=True, nullable=False)
     first_name = DB.Column(DB.String(80), nullable=False)
     last_name = DB.Column(DB.String(80), nullable=False)
-    trips = DB.relationship('UserTrip', backref='user', lazy=True)
-    activities = DB.relationship('UserActivity', backref='user', lazy=True)
+    trips = DB.relationship('Trip', backref='user', lazy=True)
+    activities = DB.relationship('Activity', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.id
 
-class UserTrip(DB.Model):
-    ''' UserTrip model '''
+class Trip(DB.Model):
+    ''' Trip model '''
     id = DB.Column(DB.Integer, primary_key=True)
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
-
+    trip_name = DB.Column(DB.String(100), unique=False, nullable=False)
+    join_code = DB.Column(DB.String(7), unique=True, nullable=False)
+    owner_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    activities = DB.relationship('Activity', backref='trip', lazy=True)
+    
     def __repr__(self):
-        return '<UserTrip %r>' % self.id
-
-class UserActivity(DB.Model):
-    ''' UserActivity model '''
+        return '<Trip %r>' % self.trip_name
+        
+class Activity(DB.Model):
+    ''' Activity model '''
     id = DB.Column(DB.Integer, primary_key=True)
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
-    user_paid = DB.Column(DB.Boolean, nullable=False)
-
+    trip_id = DB.Column(DB.Integer, DB.ForeignKey('trip.id'), nullable=False)
+    activity_name = DB.Column(DB.String(100), unique=False, nullable=False)
+    total_sum = DB.Column(DB.Integer, nullable=False)
+    total_users = DB.Column(DB.Integer, nullable=False)
+    owner_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    
     def __repr__(self):
-        return '<UserActivity %r>' % self.id
+        return '<Activity %r>' % self.activity_name
