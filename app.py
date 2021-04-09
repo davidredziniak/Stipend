c6fc92c912ecacbc66abc218ee402ae0e1f64d15
David Redziniak <ec2-user@ip-172-31-35-129.ec2.internal>
2021-04-09 20:00:21 +0000

    Edited models and added relationships between tables.

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ import models
 def index(filename):
     return send_from_directory('./build', filename)
 
-
 APP.run(
     host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', "8081")),
 )
--- models.py
+++ models.py
@@ -6,8 +6,10 @@ class User(DB.Model):
     email = DB.Column(DB.String(80), unique=True, nullable=False)
     first_name = DB.Column(DB.String(80), nullable=False)
     last_name = DB.Column(DB.String(80), nullable=False)
-    trips = DB.relationship('Trip', backref='user', lazy=True)
-    activities = DB.relationship('Activity', backref='user', lazy=True)
+    owned_trips = DB.relationship('Trip', backref='user', lazy=True)
+    owned_activities = DB.relationship('Activity', backref='user', lazy=True)
+    trips = DB.relationship('TripUser', backref='user', lazy=True)
+    activities = DB.relationship('ActivityUser', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.id
@@ -19,18 +21,37 @@ class Trip(DB.Model):
     join_code = DB.Column(DB.String(7), unique=True, nullable=False)
     owner_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
     activities = DB.relationship('Activity', backref='trip', lazy=True)
-    
+    users = DB.relationship('TripUser', backref='trip', lazy=True)
+
     def __repr__(self):
-        return '<Trip %r>' % self.trip_name
-        
+        return '<Trip %r>' % self.id
+
+class TripUser(DB.Model):
+    ''' TripUser model '''
+    id = DB.Column(DB.Integer, primary_key=True)
+    trip_id = DB.Column(DB.Integer, DB.ForeignKey('trip.id'), nullable=False)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<TripUser %r>' % self.id
+
 class Activity(DB.Model):
     ''' Activity model '''
     id = DB.Column(DB.Integer, primary_key=True)
     trip_id = DB.Column(DB.Integer, DB.ForeignKey('trip.id'), nullable=False)
     activity_name = DB.Column(DB.String(100), unique=False, nullable=False)
-    total_sum = DB.Column(DB.Integer, nullable=False)
+    total_sum = DB.Column(DB.Float, nullable=False)
     total_users = DB.Column(DB.Integer, nullable=False)
     owner_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
-    
+
     def __repr__(self):
-        return '<Activity %r>' % self.activity_name
+        return '<Activity %r>' % self.id
+
+class ActivityUser(DB.Model):
+    ''' ActivityUser model '''
+    id = DB.Column(DB.Integer, primary_key=True)
+    activity_id = DB.Column(DB.Integer, DB.ForeignKey('activity.id'), nullable=False)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<ActivityUser %r>' % self.id
