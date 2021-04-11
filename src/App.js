04b3299b8128d8f64c1108d32134e8bbc63514d0
Neel Patel <ec2-user@ip-172-31-83-156.ec2.internal>
2021-04-11 19:24:22 +0000

    Added the Route and switch for redirect

--- src/App.js
+++ src/App.js
@@ -3,29 +3,41 @@ import React, { useState } from 'react';
 import './App.css';
 import Login from'./Login.js';
 import Logout from'./Logout.js';
-import Dashboard from './Dashboard';
 
-function App() {
+import {BrowserRouter as Router, Switch,Route,Link} from "react-router-dom";
+
+export default function App() {
   
-  // const [login, setLogin] = useState(false);
-  // const [name, setName] = useState('');
-  
-  // const log = (username) => {
-  //   setNewUser(username);
-  //   setLogin(!login);
-  // };
-  
-// if(name != ''){
-//   return (name);
-// }
-// else{
-    return (
-      <div className="App">
-      <h1 className='heading'><i>STIPEND, Trips Made Easy </i></h1>
-        <Login />
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/Logout">Logout</Link>
+            </li>
+
+
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+          <Route path="/Logout">
+            <Logout />
+          </Route>
+
+        </Switch>
       </div>
-    );
-  }
-//}
+    </Router>
+  );
+}
 
-export default App;
+
