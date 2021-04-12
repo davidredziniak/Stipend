d78c3b9cac229855c0c138fcf296837be5a948b9
Shanil Rahul Pathak <ec2-user@ip-172-31-65-24.ec2.internal>
2021-04-12 01:08:08 +0000

    login has no conditional return

--- src/Login.js
+++ src/Login.js
@@ -1,42 +1,20 @@
 import React, { useState } from 'react';
 import dotenv from 'dotenv';
-<<<<<<< HEAD
-import Logout from'./Logout.js';
-import {GoogleLogin,GoogleLogout} from 'react-google-login';
-import { Redirect } from 'react-router-dom';
-=======
 import {GoogleLogin} from 'react-google-login';
 
->>>>>>> 6a3cb9bf7b3376a63647fad176c5c91428e3e397
 import {refreshTokenSetup} from './refreshToken.js';
 import {loginApi, userApi} from './api/api.js';
 import Dashboard from './Dashboard';
 
-<<<<<<< HEAD
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-
-const clientId = process.env.REACT_APP_CLIENT_ID;
-
-function Login()
-=======
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
 function Login(props)
->>>>>>> bee57b52b63726c0427716929fd1d905898280a7
 {
     const [email,setEmail] = useState('');
     const [name,setName] = useState('');
     const [tokenId, setTokenId] = useState('');
     const [givenName,setGivenName] = useState('');
     const [logStatus,setLogStatus] = useState(false);
-    const [tokenId,setTokenId] = useState('');
-    console.log(logStatus);
     
     
     const onSuccess= (res)=>
@@ -56,27 +34,8 @@ function Login(props)
     {
         console.log('[Login failed] res: ',res);
     }
-<<<<<<< HEAD
-    //<Dashboard email={email} name={name} givenName={givenName} setLogStatus={setLogStatus}/>
-    if(logStatus){
-        return (
-
-            <Router>
-                <div>
-                    <Switch>
-                        <Route exact path="/" render={(props) => ( <Dashboard email={email} name={name} givenName={givenName} setLogStatus={setLogStatus} /> )}/>
-                    </Switch>
-                </div>
-            </Router>
-            );
-
-    }
-    else{
-        return (
-=======
 
     return (
->>>>>>> bee57b52b63726c0427716929fd1d905898280a7
             <div>
                 <div>
                 <GoogleLogin
--- src/Logout.js
+++ src/Logout.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import dotenv from 'dotenv'
 import Login from'./Login.js';
-<<<<<<< HEAD
-import {GoogleLogout} from 'react-google-login';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-=======
 import { GoogleLogout } from 'react-google-login';
->>>>>>> bee57b52b63726c0427716929fd1d905898280a7
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
@@ -26,24 +16,12 @@ function Logout(props)
     
     return(
         <div>
-<<<<<<< HEAD
-            <div>
-                
-=======
->>>>>>> bee57b52b63726c0427716929fd1d905898280a7
                 <GoogleLogout
                 clientId={clientId}
                 buttonText="Logout"
                 onLogoutSuccess={onSuccess}
                 style ={{marginTop: '100px'}}
-                to='/login'
                 />
-<<<<<<< HEAD
-        
-                
-            </div>
-=======
->>>>>>> bee57b52b63726c0427716929fd1d905898280a7
         </div>
         );
 }
