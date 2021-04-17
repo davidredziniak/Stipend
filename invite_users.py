2cdd12feeca79d6299d0610e2132c09bf3484601
Mervyn Mathew <mervyn.mathew18@gmail.com>
2021-04-17 20:30:52 +0000

    Emails are now formatted in html via MIMEText, created callable method

--- invite_users.py
+++ invite_users.py
@@ -1,20 +1,53 @@
 import os
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())  # This is to load your env variables from .env
 
-port = 465  # For SSL
-smtp_server = "smtp.gmail.com"
-sender_email = os.getenv('G_EMAIL')  # Enter your address
-receiver_email = "mm2373@njit.edu"
-password = os.getenv('G_EMAIL_PASS')
-message = """\
-Subject: Hi there
+PORT = 465  # For SSL
+SMTP_SERVER = "smtp.gmail.com"
+SENDER = os.getenv('G_EMAIL')
+WEBSITE_LINK = "INSERT_LINK_HERE"
 
-This message is sent from Python."""
+def create_invite_message(inviter_name, join_code):
+    message = '''\
+    <html>
+        <body>
+            <p>
+                ''' + inviter_name + ''' has invited you to plan the expenses of your upcoming trip with Stipend!<br>
+                Instructions:<br>
+            </p>
+            <ol type="1">
+                <li>Join site via link: ''' + WEBSITE_LINK + '''</li>
+                <li>Sign in using a Google Account</li>
+                <li>Once signed in, press the "Join Trip" button on the top navigation bar</li>
+                <li>Inpute the join code: ''' + join_code + '''</li>
+            </ol>
+            <p><br>
+                We look forward to helping you plan your trip!
+            </p>
+        </body>
+    </html>
+    '''
+    
+    return message
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+def create_email(inviter_name):
+    message = MIMEMultipart('alternative')
+    message["Subject"] = inviter_name + ' has invited you to plan a trip!'
+    message["From"] = SENDER
+    
+    return message
+
+def send_invites(inviter_name, invitees, join_code):
+    html_message = create_invite_message(inviter_name, join_code)
+    message = create_email(inviter_name)
+    email_body = MIMEText(html_message, "html")
+    message.attach(email_body)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=context) as server:
+        server.login(SENDER, os.getenv('G_EMAIL_PASS'))
+        for invitee in invitees:
+            server.sendmail(SENDER, invitee, message.as_string())
