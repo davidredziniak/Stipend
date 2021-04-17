7952d14d237f82e1ad2fabf39bc419eae8d63a84
Mervyn Mathew <mervyn.mathew18@gmail.com>
2021-04-17 20:46:56 +0000

    Updated formatting and added docstrings to pass pylint

--- invite_users.py
+++ invite_users.py
@@ -1,5 +1,9 @@
+'''
+    Emails the invited users with the given join code on behalf of the inviter.
+'''
 import os
-import smtplib, ssl
+import smtplib
+import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv, find_dotenv
@@ -12,11 +16,15 @@ SENDER = os.getenv('G_EMAIL')
 WEBSITE_LINK = "INSERT_LINK_HERE"
 
 def create_invite_message(inviter_name, join_code):
+    '''
+        Creates an html email body with the given inviter's name and join code.
+    '''
     message = '''\
     <html>
         <body>
             <p>
-                ''' + inviter_name + ''' has invited you to plan the expenses of your upcoming trip with Stipend!<br>
+                ''' + inviter_name + ''' has invited you to plan the expenses
+                of your upcoming trip with Stipend!<br>
                 Instructions:<br>
             </p>
             <ol type="1">
@@ -31,17 +39,28 @@ def create_invite_message(inviter_name, join_code):
         </body>
     </html>
     '''
-    
+
     return message
 
 def create_email(inviter_name):
+    '''
+        Creates a MIMEMultipart message and sets headers.
+    '''
     message = MIMEMultipart('alternative')
     message["Subject"] = inviter_name + ' has invited you to plan a trip!'
     message["From"] = SENDER
-    
+
     return message
 
 def send_invites(inviter_name, invitees, join_code):
+    '''
+        Sends out emails to invitees with the join code.
+
+        Args:
+            inviter_name: the name of the user who is inviting invitees.
+            invitees: a list of emails of users to invite.
+            join_code: the join code corresponding to the trip inviter is inviting invitees to.
+    '''
     html_message = create_invite_message(inviter_name, join_code)
     message = create_email(inviter_name)
     email_body = MIMEText(html_message, "html")
