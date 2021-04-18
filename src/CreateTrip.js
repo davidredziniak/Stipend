d976e09881802b3d667feb24bac2a62409eac922
Neel Patel <ec2-user@ip-172-31-83-156.ec2.internal>
2021-04-18 19:48:33 +0000

    commit before pull

--- src/CreateTrip.js
+++ src/CreateTrip.js
@@ -3,7 +3,10 @@ import TripHome from './TripHome';
 import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 import './App.css';
+import moment from 'moment';
+//import DatePicker from 'react-datetime';
 import { BrowserRouter as Router,Route, Link} from "react-router-dom";
+//import 'react-datetime/css/react-datetime.css';
 /* eslint-disable react/jsx-props-no-spreading */
 function CreateTrip(props){
 
@@ -19,7 +22,11 @@ function CreateTrip(props){
       setDataa(true);
       console.log(dataa)
   }
-  
+//     // disable past dates
+//   const yesterday = moment().subtract(1, 'day');
+//   const disablePastDt = current => {
+//     return current.isAfter(yesterday);
+//   };
   console.log(errors);
 
     return(
@@ -31,7 +38,8 @@ function CreateTrip(props){
                       <div><label className="StartDate">Enter the Trip Name: </label><input type="text"  placeholder="Trip Name" {...register("Trip Name", {required: true, maxLength: 17})} /></div>
                       <div><label className="StartDate">Enter Email to Invite: </label><input type="text"  placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} /></div>
                       <div><label className="StartDate">Start Date: </label><input type="date"  placeholder="Start Date" {...register("Start Date", {required: true})} /></div>
-                      <div><label className="StartDate">End Date: </label><input type="date"  placeholder="End Date" {...register("End Date", {required: true})} /></div>
+                      <div><label className="StartDate">End Date: </label>  
+                         <input type="date" placeholder="End Date" {...register("End Date", {required: true})} /></div>
                        {dataa?(<div><Link to="/TripHome"><input type="submit" /></Link></div>):<div><input type="submit"/></div>}
                 </form>
                    
