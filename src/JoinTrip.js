915a4a891962aab8bd0360fe7f45a0c65eac2f4d
Neel Patel <ec2-user@ip-172-31-83-156.ec2.internal>
2021-04-28 17:19:15 +0000

    fixed the jointrip button bug

--- src/JoinTrip.js
+++ src/JoinTrip.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { joinTripApi } from './api/api.js';
 import { useHistory} from "react-router-dom";
 import './App.css';
@@ -6,19 +6,24 @@ import './App.css';
 function JoinTrip(props){
     const user = useRef('');
     const history = useHistory();
-    
+    const [check,setCheck] =useState(true); 
     function joinTrip(joinCode){
-        if(props.token !== ""){
+        if(props.token !== ""  && joinCode!=""){
             joinTripApi(props.token, joinCode).then(data => history.push('/trip/' + data.tripId));
+            if(user==null || user=="" || user==undefined){
+            setCheck(false);
+            }
+            user.current.value=""
         }
     }
-    
     return(
         <div className="activity">
              {props.isAuth ?
              (<div><h3 className="headingClass">Join an existing trip!</h3>
-              <input required type="text" placeholder="Enter Invitation Code"  className = "joinTrip" ref={user} pattern="^[a-zA-Z0-9]*$" maxLength="7"/>
-              <div ><button className = "joinSubmit" onClick={()=>joinTrip(user.current.value)} type="submit" required>Join Trip</button></div>
+             <form>
+                <input type="text" placeholder="Enter Invitation Code" className = "joinTrip" ref={user} pattern="^[a-zA-Z0-9]*$" maxLength="7" required/>
+                <div><button className = "joinSubmit" onClick={()=>joinTrip(user.current.value)} type="submit">Join Trip</button></div>
+             </form>
              </div>)
              :null}
         </div>
