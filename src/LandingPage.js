8241c17736bce2cefe22b0b281cffb09a505b741
Neel Patel <ec2-user@ip-172-31-83-156.ec2.internal>
2021-04-29 23:17:18 +0000

    updated landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,14 +4,21 @@ import './App.css';
 function LandingPage()
 {
     return (
+        <body className="imageClass">
         <div>
         <h1>Welcome to STIPEND!</h1>
         <h3>Basic Functionality  of the APP</h3>
         <ul className="LandingPage">
             <li>Helps you manage the trip by splitting the money</li>
             <li>Helps you financially plans your trip</li>
+
         </ul>
         </div>
+        </body>
+        
+        
+        
+        
         );
 }
 
