e89793874d0ad98c5b9941a5b99c6adb0580f97a
Neel Patel <ec2-user@ip-172-31-83-156.ec2.internal>
2021-04-30 05:10:22 +0000

    updated an landing page witht he team information

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Logo.css';
 import './App.css';
 
 function LandingPage()
@@ -19,10 +20,64 @@ function LandingPage()
             </div>
         
             <div>
-                <p>Hello</p>
-                <p>Hello</p>
-                <p>Hello</p>
-                <p>Hello</p>
+            <h1>OUR TEAM </h1>
+                <div class="row">
+                  <div class="column">
+                    <div class="card">
+                      <div class="container">
+                      <img alt="Qries" src="https://cdn.dribbble.com/users/3293507/screenshots/14667603/media/d8cbe035a61f64afdf6deabca5182842.jpg" width="40" height="50"/>
+                        <h2>David Redziniak</h2>
+                        <p class="title">CEO & Founder</p>
+                        <p>Email:- dr475@njit.edu</p>
+                        <p><button class="button">Contact</button></p>
+                      </div>
+                    </div>
+                  </div>
+                
+               
+                
+                
+                  <div class="column">
+                    <div class="card">
+                      <div class="container">
+                      <img alt="Qries" src="https://cdn.dribbble.com/users/3293507/screenshots/14667603/media/d8cbe035a61f64afdf6deabca5182842.jpg" width="50" height="50"/>
+                        <h2>Shanil Rahul Pathak</h2>
+                        <p class="title">CEO & Founder</p>
+                        <p>Email:- srp93@njit.edu</p>
+                        <p><button class="button">Contact</button></p>
+                      </div>
+                    </div>
+                  </div>
+                
+             
+                  <div class="column">
+                    <div class="card">
+                      <div class="container">
+                      <img alt="Qries" src="https://cdn.dribbble.com/users/3293507/screenshots/14667603/media/d8cbe035a61f64afdf6deabca5182842.jpg" width="50" height="50"/>
+                        <h2>Mervyn Mathew</h2>
+                        <p class="title">CEO & Founder</p>
+                        <p>Email:- mm2373@njit.edu</p>
+                        <p><button class="button">Contact</button></p>
+                      </div>
+                    </div>
+                  </div>
+                
+               
+                
+                
+                  <div class="column">
+                    <div class="card">
+                      <div class="container">
+                      <img alt="Qries" src="https://cdn.dribbble.com/users/3293507/screenshots/14667603/media/d8cbe035a61f64afdf6deabca5182842.jpg" width="50" height="50"/>
+                        <h2>Neel Patel</h2>
+                        <p class="title">CEO & Founder</p>
+                        <p>Email:- ngp23@njit.edu</p>
+                        <p><button class="button">Contact</button></p>
+                      </div>
+                    </div>
+                  </div>
+                
+                </div>
             </div>
         
         </div>
